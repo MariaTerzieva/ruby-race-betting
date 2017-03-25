50398ee6880b5e97890c4bd0675e1c53b2e46887
Maria Terzieva <mariya.g.terzieva@gmail.com>
2017-03-25 16:56:25 +0200

    Add solution

--- lib/race.rb
+++ lib/race.rb
@@ -3,7 +3,18 @@ module RaceBet
 
     class << self
       def score(guesses, winners)
+        matches = guesses.zip(winners).first(5).map { |guess, winner| guess == winner }
+        points = [15, 10, 5, 3, 1]
 
+        matches.each_with_index.map do |match, index|
+          if match
+            points[index]
+          elsif winners.include? guesses[index]
+            points[4]
+          else
+            0
+          end
+        end.inject(:+)
       end
     end
 
